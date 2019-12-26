c23e49d54a6890ebdb9b43699cbd7629363560ef
alexisdavalos <alexisdavalos.tech@gmail.com>
2019-12-26 15:29:49 +0100

    Added useState Hook to Login Form

--- evends/src/components/Login_Register/Login_Register.js
+++ evends/src/components/Login_Register/Login_Register.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import {Wrapper, Form_S, FormGroup_S, Logo, Title} from './Login_Register_Styles';
 import {Link} from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 
 const Login_Register = () => {
+
+const [Login, setLogin] = useState(true);
+
+const Register = () =>{
+    setLogin(false)
+}
+const Login_set = () =>{
+    setLogin(true)
+}
+if (Login === true){
     return(
         <>
         <Navbar/>
         <Wrapper>
             <Logo src='/evends.png'></Logo>
             <Form_S>
-            <Title>Welcome To Evends</Title>
+            <Title>Login To Evends</Title>
                 <FormGroup_S>
                     <Input
                         id='username'
@@ -29,15 +39,50 @@ const Login_Register = () => {
                     />
                 </FormGroup_S>
                 <FormGroup_S>
-                    <Button>Login</Button>
-                    <p>or</p>
-                    <Link>Register</Link>
+                    <Button onClick={Login_set}>Login</Button>
+                    <span style={{margin:'0px 5px'}}>or</span>
+                    <Button onClick={Register}>Register</Button>
                 </FormGroup_S>
            
        </Form_S>
         </Wrapper>
        </>
     );
+} else{
+    return(
+        <>
+        <Navbar/>
+        <Wrapper>
+            <Logo src='/evends.png'></Logo>
+            <Form_S>
+            <Title>Register For Evends</Title>
+                <FormGroup_S>
+                    <Input
+                        id='username'
+                        type='text'
+                        name='username'
+                        placeholder='Enter Username'
+                    />
+                </FormGroup_S>
+                <FormGroup_S>
+                    <Input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Enter Password" 
+                    />
+                </FormGroup_S>
+                <FormGroup_S>
+                    <Button onClick={Login_set}>Login</Button>
+                    <span style={{margin:'0px 5px'}}>or</span>
+                    <Button onClick={Register}>Register</Button>
+                </FormGroup_S>
+           
+       </Form_S>
+        </Wrapper>
+       </>
+    );
+}
 }
 
 export default Login_Register;
