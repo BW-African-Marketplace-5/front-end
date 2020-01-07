9f012a2465df31657ec530afaecfc323d2ee2c88
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-01-07 23:37:14 +0100

    Updated Footer

--- src/components/Footer/Footer_Styles.jsx
+++ src/components/Footer/Footer_Styles.jsx
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 export const FooterContainer = styled.div`
     background-color:#e84c3d;
     width: 100%;
-    padding: 5%;
+    margin-top:10%;
+    padding: 1%;
     display:flex;
     justify-content:center;
     align-items:center;
+    p{
+        color:white;
+        font-weight:bold;
+    }
 
 `
