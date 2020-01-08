5bf2d3501c54e38720567364012109f1ebec8717
Robbie Porter <rthomporter@gmail.com>
2020-01-08 16:11:07 -0600

    currency update

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,55 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
-  ItemWrapper, 
-  ProductCard, 
+  ItemWrapper,
+  ProductCard,
   Title,
   SubTitle,
   Price
-} from './ItemCard_Styles';
-import {
-   CardText, 
-   CardBody,  
-   Button
-} from 'reactstrap';
-import {
-   GiGrain,
-   GiMeat,
-   GiFruitBowl,
-   GiFruiting
-} from "react-icons/gi";
-import {FaDollarSign} from 'react-icons/fa'
+} from "./ItemCard_Styles";
+import { CardText, CardBody, Button } from "reactstrap";
+import { GiGrain, GiMeat, GiFruitBowl, GiFruiting } from "react-icons/gi";
+import { FaDollarSign } from "react-icons/fa";
 const ItemCard = props => {
   const [iconState, setIcon] = useState();
-  console.log('The Category Is:', props.data.category)
-  console.log('The Icons are:',iconState);
+  console.log("The Category Is:", props.data.category);
+  console.log("The Icons are:", iconState);
 
-  useEffect(() =>{
-    if(props.data.category === 'Grains'){
-      setIcon(<GiGrain/>)
+  useEffect(() => {
+    if (props.data.category === "Grains") {
+      setIcon(<GiGrain />);
     }
-    if(props.data.category === 'Meats'){
-      setIcon(<GiMeat/>)
+    if (props.data.category === "Meats") {
+      setIcon(<GiMeat />);
     }
-    if(props.data.category === 'Vegetables'){
-      setIcon(<GiFruiting/>)
+    if (props.data.category === "Vegetables") {
+      setIcon(<GiFruiting />);
     }
-    if(props.data.category === 'Fruits'){
-      setIcon(<GiFruitBowl/>)
+    if (props.data.category === "Fruits") {
+      setIcon(<GiFruitBowl />);
     }
-  },[])
-  
- 
+  }, []);
 
   return (
     <ProductCard>
       <Title>{props.data.name}</Title>
-      <CardBody>            
-        <SubTitle>Category: {iconState} {props.data.category}</SubTitle>
+      <CardBody>
+        <SubTitle>
+          Category: {iconState} {props.data.category}
+        </SubTitle>
         <SubTitle>Market Area: {props.data.market_area}</SubTitle>
         <SubTitle>Description: {props.data.description}</SubTitle>
         <CardText></CardText>
       </CardBody>
-      <Price>Price: <FaDollarSign/> {props.data.price}</Price>
+      <Price>Price: {props.data.price}</Price>
     </ProductCard>
   );
 };
--- src/components/Navbar/LoggedinNav.js
+++ src/components/Navbar/LoggedinNav.js
@@ -1,12 +1,17 @@
 import React from "react";
-import {Navigation, Logo, MenuLink, LinkWrapper, NavWrapper } from './Navbar_Styles';
-import logo from '../../imgs/evends.png'
+import {
+  Navigation,
+  Logo,
+  MenuLink,
+  LinkWrapper,
+  NavWrapper
+} from "./Navbar_Styles";
+import logo from "../../imgs/evends.png";
 const Navbar = () => {
-    return(
-  
-        <Navigation>
-        <NavWrapper>
-          <Logo src={logo}></Logo> 
+  return (
+    <Navigation>
+      <NavWrapper>
+        <Logo src={logo}></Logo>
         <LinkWrapper>
           <MenuLink href="/">Home</MenuLink>
         </LinkWrapper>
@@ -17,11 +22,13 @@ const Navbar = () => {
           <MenuLink href="/item-list">Inventory</MenuLink>
         </LinkWrapper>
         <LinkWrapper>
-          <MenuLink href="/">Log Out</MenuLink>
+          <MenuLink onClick={() => localStorage.clear("token")} href="/">
+            Log Out
+          </MenuLink>
         </LinkWrapper>
-        </NavWrapper>
-        </Navigation>
-    );
-}
+      </NavWrapper>
+    </Navigation>
+  );
+};
 
 export default Navbar;
